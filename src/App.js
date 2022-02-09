4256863c05e0936c7946d2c458929fdf6bba30ec
Kirill <vintik.kirill@gmail.com>
2022-02-09 16:51:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'Bread'},
+    {id: 2, completed: false, title: 'Butter'},
+    {id: 3, completed: false, title: 'Milk'}
+  ])
+
+
+  const toggleToDo = id => {
+    setTodos(
+      todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })
+    )
+  }
+
   return (
-    <div className='wrapper' />
+    <div className='wrapper'>
+    <h1>React tutorial</h1>
+
+    <TodoList todos={todos} onToggle={toggleToDo}/>
+    </div>
   )
 }
 
